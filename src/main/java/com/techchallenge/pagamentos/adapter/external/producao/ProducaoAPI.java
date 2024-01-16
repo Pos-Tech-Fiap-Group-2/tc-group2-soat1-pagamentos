5f730e6859a395e7f4a9f7005e54796721f9d7f1
Viviane Scarlatti <viviane.scarlatti@gmail.com>
2024-01-16 19:17:07 -0300

    Update url

--- src/main/java/com/techchallenge/pagamentos/adapter/external/producao/ProducaoAPI.java
+++ src/main/java/com/techchallenge/pagamentos/adapter/external/producao/ProducaoAPI.java
@@ -3,7 +3,7 @@ package com.techchallenge.pagamentos.adapter.external.producao;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(url = "http://localhost:8080")
+@FeignClient(url = "${producao.api.url}")
 public interface ProducaoAPI {
     @PostMapping(value = "/api/producao/{pedidoId}/adicionar")
     public void adicionarPedidoFilaProducao(@PathVariable String pedidoId);
